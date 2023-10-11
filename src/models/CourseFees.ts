82a23b7a461aeef71dbdb69fe63bb317c4830f00
Vaibhav <dev.vaibhav99@gmail.com>
2023-10-11 14:29:24 +0530

    CourseFees model added

---
+++ src/models/CourseFees.ts
@@ -0,0 +1,9 @@
+export interface CourseFees {
+  userId: number;
+  studentId: number;
+  revenueCategoryId: number;
+  amount: number;
+  totalFees: number;
+  paidFees: number;
+  balanceFees: number;
+}
