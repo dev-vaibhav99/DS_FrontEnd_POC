d452ba0d377348afc069050ab03b8ede4d26c598
Vaibhav <dev.vaibhav99@gmail.com>
2023-10-11 09:42:39 +0530

    service folder added containing revenue.service.ts file

---
+++ src/services/revenue.service.ts
@@ -0,0 +1,80 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:3000/api/v1/revenue";
+
+interface Item {
+  id: number;
+  name: string;
+  // Add other properties here
+}
+
+interface CourseFees {
+  userId: number;
+  studentId: number;
+  revenueCategoryId: number;
+  amount: number;
+  totalFees: number;
+  paidFees: number;
+  balanceFees: number;
+}
+
+interface OtherIncome {
+  userId: number;
+  revenueCategoryId: number;
+  amount: number;
+}
+
+interface MentorSalaryExpense {
+  revenueCategoryId: number;
+  amount: number;
+  mentorId: number;
+  remark: string;
+}
+
+interface OtherExpense {
+  revenueCategoryId: number;
+  amount: number;
+  remark: string;
+}
+
+const apiService = {
+  getAllRevenueCategories: (): Promise<any> => axios.get(`${BASE_URL}/`),
+
+  getRevenueCategoryById: (id: number): Promise<any> =>
+    axios.get(`${BASE_URL}/${id}`),
+
+  createRevenueCategory: (revenueCategoryData: Item): Promise<any> =>
+    axios.post(`${BASE_URL}/add`, revenueCategoryData),
+
+  updateRevenueCategory: (
+    id: number,
+    revenueCategoryData: Item
+  ): Promise<any> => axios.put(`${BASE_URL}/update/${id}`, revenueCategoryData),
+
+  deleteRevenueCategoryById: (id: number): Promise<any> =>
+    axios.delete(`${BASE_URL}/delete/${id}`),
+
+  addCourseFeesIncomeDetails: (incomeDetails: CourseFees): Promise<any> =>
+    axios.post(`${BASE_URL}/payment`, incomeDetails),
+
+  addOtherIncomeDetails: (incomeDetails: OtherIncome): Promise<any> =>
+    axios.post(`${BASE_URL}/payment`, incomeDetails),
+
+  getAllIncomeDetails: (): Promise<any> => axios.get(`${BASE_URL}/income`),
+
+  getIncomeDetailsById: (id: number): Promise<any> =>
+    axios.get(`${BASE_URL}/income/${id}`),
+
+  addMentorFeesExpense: (expenseDetails: MentorSalaryExpense): Promise<any> =>
+    axios.post(`${BASE_URL}/expense`, expenseDetails),
+
+  addOtherExpense: (expenseDetails: OtherExpense): Promise<any> =>
+    axios.post(`${BASE_URL}/expense`, expenseDetails),
+
+  getAllExpenseDetails: (): Promise<any> => axios.get(`${BASE_URL}/expenses`),
+
+  getExpenseDetailsById: (id: number): Promise<any> =>
+    axios.get(`${BASE_URL}/expense/${id}`),
+};
+
+export default apiService;
