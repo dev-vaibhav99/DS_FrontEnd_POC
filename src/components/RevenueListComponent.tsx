589d5b346e57f89fa5b2d4edb757f5ea3860e8c2
Vaibhav <dev.vaibhav99@gmail.com>
2023-10-11 09:43:21 +0530

    RevenueListComponent added to display all the revenue categories

---
+++ src/components/RevenueListComponent.tsx
@@ -0,0 +1,34 @@
+// ItemList.tsx
+import React, { useEffect, useState } from "react";
+import apiService from "../services/revenue.service";
+
+interface Item {
+  id: number;
+  name: string;
+  // Add other properties here
+}
+
+const RevenueListComponent: React.FC = () => {
+  const [items, setItems] = useState<Item[]>([]);
+
+  useEffect(() => {
+    // Fetch items from the API when the component mounts
+    apiService.getAllRevenueCategories().then((data) => {
+      console.log(data.data);
+      setItems(data.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Item List</h1>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RevenueListComponent;
